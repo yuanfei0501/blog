bda32e123b54cf4573f2a6df337e5efeca349021
yuanfei <yuanfei@yuanxinjituan.com>
2023-07-11 13:42:30 +0800

    初始化

--- deploy.sh
+++ deploy.sh
@@ -46,6 +46,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/yuanfei0501/vuepress-blog-yf.git master:gh-pages
+git push -f git@github.com:yuanfei0501/vuepress-blog-yf.git main:gh-pages
 
 cd -
